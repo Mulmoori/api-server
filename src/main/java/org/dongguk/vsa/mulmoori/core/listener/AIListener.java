47bdb68dec6beccada0e20bbbc5d91d7b364a4f4
HyungJoon <kr.dev.hyungjoon@gmail.com>
2024-08-20 06:36:47 +0900

    feat: EDA Structure

--- src/main/java/org/dongguk/vsa/mulmoori/core/listener/AIListener.java
+++ src/main/java/org/dongguk/vsa/mulmoori/core/listener/AIListener.java
@@ -2,26 +2,68 @@ package org.dongguk.vsa.mulmoori.core.listener;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.dongguk.vsa.mulmoori.core.utility.AnalysisUtil;
+import org.dongguk.vsa.mulmoori.dialogue.domain.Dialogue;
+import org.dongguk.vsa.mulmoori.dialogue.domain.type.EDialogueStatus;
 import org.dongguk.vsa.mulmoori.dialogue.event.CreateDialogueEvent;
 import org.dongguk.vsa.mulmoori.dialogue.event.UpdateAnswerInDialogueEvent;
+import org.dongguk.vsa.mulmoori.dialogue.repository.DialogueRepository;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class AIListener {
 
+    private final AnalysisUtil analysisUtil;
+
+    private final DialogueRepository dialogueRepository;
+
     @Async
     @EventListener(classes = {CreateDialogueEvent.class})
     public void handleCreateDialogueEvent(CreateDialogueEvent event) {
-        // TODO: AI Server 와 통신이 필요함
+        String answer = null;
+
+        try {
+            answer = analysisUtil.createAnswer(
+                    event.id(),
+                    event.question()
+            );
+        } catch (Exception ignored) {
+        }
+
+        Optional<Dialogue> dialogue = dialogueRepository.findById(event.id());
+
+        if (dialogue.isEmpty()) {
+            return;
+        }
+
+        if (answer == null) {
+            dialogue.get().updateStatus(EDialogueStatus.PROGRESS);
+        } else {
+            dialogue.get().updateAnswer(
+                    answer,
+                    true,
+                    EDialogueStatus.COMPLETED
+            );
+        }
     }
 
     @Async
     @EventListener(classes = {UpdateAnswerInDialogueEvent.class})
     public void handleUpdateAnswerInDialogueEvent(UpdateAnswerInDialogueEvent event) {
-        // TODO: AI Server 와 통신이 필요함
+        try {
+            analysisUtil.updateVectorStore(
+                    event.id(),
+                    event.question(),
+                    event.answer()
+            );
+        }catch (Exception ignored){
+        }
     }
 }
--- src/main/java/org/dongguk/vsa/mulmoori/dialogue/event/UpdateAnswerInDialogueEvent.java
+++ src/main/java/org/dongguk/vsa/mulmoori/dialogue/event/UpdateAnswerInDialogueEvent.java
@@ -4,7 +4,7 @@ import lombok.Builder;
 
 @Builder
 public record UpdateAnswerInDialogueEvent(
-        Long dialogueId,
+        Long id,
         String question,
         String answer
 ) {
